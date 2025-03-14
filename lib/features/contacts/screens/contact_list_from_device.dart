f3ab62f27620266b522903f7bec44ad1fdc06dbe
Jürgen Hollmann <h.juergen.hollmann@gmail.com>
2025-03-14 19:47:16 +0100

    added colors and borders to "ContactListFromDevice"

--- lib/features/contacts/screens/contact_list_from_device.dart
+++ lib/features/contacts/screens/contact_list_from_device.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
+import 'package:workbuddy/config/wb_colors.dart';
 // import 'package:workbuddy/config/wb_colors.dart';
 // import 'package:workbuddy/config/wb_dialog_2buttons.dart';
 
@@ -68,48 +69,174 @@ class _ContactListFromDeviceState extends State<ContactListFromDevice> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Kontakte'),
-      ),
-      body: Column(
-        children: [
-          // Suchfeld
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
+          title: Text(
+            'Kontakte im Gerät',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: wbColorBackgroundBlue),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Column(
+          children: [
+            // Anzeige der Anzahl der Kontakte
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+              child: Text(
+                'Gefunden: ${filteredContacts.length} von insgesamt ${allContacts.length}',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Suchfeld
+            TextField(
               controller: _searchController,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w900,
+              ),
               decoration: InputDecoration(
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+                  child: Icon(
+                    Icons.search_outlined,
+                    size: 40,
+                  ),
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.recycling_outlined,
+                    size: 40,
+                  ),
+                  onPressed: () {
+                    _searchController.clear();
+                  },
+                ),
                 labelText: 'Suche Kontakte',
-                border: OutlineInputBorder(),
+                labelStyle: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                ),
+                filled: true,
+                fillColor: Colors.yellow, // Innere Gelb färben
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(
+                    color: Colors.black,
+                    width: 2,
+                  ), // Schwarzer dünner Rand
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(
+                      color: Colors.black, width: 2), // Schwarzer dünner Rand
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(
+                      color: Colors.black, width: 2), // Schwarzer dünner Rand
+                ),
               ),
             ),
-          ),
-          // Liste der gefilterten Kontakte
-          Expanded(
-            child: NotificationListener<ScrollNotification>(
-              onNotification: (ScrollNotification scrollInfo) {
-                if (scrollInfo.metrics.pixels ==
-                    scrollInfo.metrics.maxScrollExtent) {
-                  loadMoreContacts();
-                }
-                return true;
-              },
-              child: ListView.builder(
-                itemCount: filteredContacts.length < currentMax
-                    ? filteredContacts.length
-                    : currentMax,
-                itemBuilder: (context, index) {
-                  final contact = filteredContacts[index];
-                  return ListTile(
-                    title: Text(contact.displayName),
-                    subtitle: Text(contact.phones.isNotEmpty
-                        ? contact.phones.first.number
-                        : 'Keine Telefonnummer'),
-                  );
+            Divider(
+              color: Colors.black,
+              thickness: 2,
+            ),
+            // wbSizedBoxHeight8,
+            // Liste der gefilterten Kontakte
+            Expanded(
+              child: NotificationListener<ScrollNotification>(
+                onNotification: (ScrollNotification scrollInfo) {
+                  if (scrollInfo.metrics.pixels ==
+                      scrollInfo.metrics.maxScrollExtent) {
+                    loadMoreContacts();
+                  }
+                  return true;
                 },
+                
+                child: ListView.builder(
+                  itemCount: filteredContacts.length < currentMax
+                      ? filteredContacts.length
+                      : currentMax,
+                  itemBuilder: (context, index) {
+                    final contact = filteredContacts[index];
+                    return Container(
+                      margin: EdgeInsets.fromLTRB(0, 0, 0, 16),
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 10,
+                            offset: Offset(0, 5),
+                          ),
+                        ],
+                        border: Border.all(
+                            color: Colors.black,
+                            width: 1), // Schwarzer dünner Rand
+                        borderRadius:
+                            BorderRadius.circular(16), // Radius von 16
+                      ),
+                      child: Card(
+                        color: wbColorBackgroundBlue, // Card rosa einfärben
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(16), // Radius von 16
+                        ),
+                        elevation: 10, // 3-D-Effekt
+                        shadowColor: Colors.black54, // Schattenfarbe
+                        child: ListTile(
+                          leading: (contact.photo != null &&
+                                  contact.photo!.isNotEmpty)
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black,
+                                        blurRadius: 5,
+                                        offset: Offset(8, 8),
+                                      ),
+                                    ],
+                                  ),
+                                  child: CircleAvatar(
+                                    backgroundImage:
+                                        MemoryImage(contact.photo!),
+                                    radius: 30,
+                                  ),
+                                )
+                              : CircleAvatar(
+                                  radius: 30,
+                                  child: Icon(Icons.person, size: 30),
+                                ),
+                          title: Text(contact.displayName,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: wbColorLogoBlue,
+                              )),
+                          subtitle: Text(
+                            contact.phones.isNotEmpty
+                                ? contact.phones.first.number
+                                : 'Keine Telefonnummer',
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: wbColorLogoBlue,
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
